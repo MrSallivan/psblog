e62f75cdb5c4a0c793a383213ddaf117a277342d
MrSallivan <nik.sallivan@gmail.com>
2021-01-20 23:05:08 +1000

    12end

---
+++ assets/js/modal.js
@@ -0,0 +1,48 @@
+const modalBtn = document.querySelectorAll('[data-modal]')
+const body = document.querySelector('body')
+const modalClose = document.querySelectorAll('.modal__close')
+const modal = document.querySelectorAll('.modal')
+
+modalBtn.forEach( item => {
+	item.addEventListener('click', event => {
+		let $this = event.currentTarget
+		let modalId = $this.getAttribute('data-modal')
+		let modal = document.getElementById(modalId)
+		let modalContent = modal.querySelector('.modal__content')
+
+		modalContent.addEventListener('click', event => {
+			event.stopPropagation()
+		})
+
+		modal.classList.add('show')
+		body.classList.add('no-scroll')
+
+		setTimeout(function() {
+			modalContent.style.transform = 'none'
+		}, 1)
+		
+	})
+})
+
+modalClose.forEach(item => {
+	item.addEventListener('click', event => {
+		let currentModal = event.target.closest('.modal')
+		closeModal(currentModal)
+	})
+})	
+
+modal.forEach(item => {
+	item.addEventListener('click', event => {
+		let currentModal = event.target
+		closeModal(currentModal)
+	})
+})
+
+function closeModal(currentModal) {
+	let modalContent = currentModal.querySelector('.modal__content')
+	modalContent.removeAttribute('style')
+	setTimeout(()=>{
+		currentModal.classList.remove('show')
+		body.classList.remove('no-scroll')
+	}, 200)
+}
